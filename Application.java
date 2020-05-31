23ef5543d528c08784399073c679c30c88b4998d
alexem55 <66222572+alexem55@users.noreply.github.com>
2020-05-31 23:48:03 +0300

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+        k_means k = new k_means();
+
+        k.runProcess();
+    }
+}
